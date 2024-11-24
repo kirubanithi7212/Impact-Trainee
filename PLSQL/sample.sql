2cb54007976d3c4f40851e35ca72a841761277d2
Kirubanithi Samy <kirubanithi.samy@aspiresys.com>
2024-11-24 22:43:00 +0530

    altered

--- PLSQL/sample.sql
+++ PLSQL/sample.sql
@@ -1,6 +1,5 @@
 use practice;
 create table products (productId int,productName varchar(20),quantity int(5),price int(5));
-select * from products;
-insert into products values(4,'power bank',5,8500);
 
-/*this procedure is for showing product details*/
+
+
